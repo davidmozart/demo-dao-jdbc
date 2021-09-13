730c2c508e1956723af69427ca8d7cc6d0216784
davidmozart <davidmozart01@gmail.com>
2021-09-13 00:51:56 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		//SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "Books");
 		//Date data = new Date(sdf.parse("22/10/2000").getTime());
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
